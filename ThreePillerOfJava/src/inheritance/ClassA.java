ab1aa02d6e731a014b843d60777078a2dd6b072d
JOHN <JOHN@DESKTOP-6AG2VUU>
2023-02-21 20:45:41 -0800

    additinal variable JIRA[456]

--- ThreePillerOfJava/src/inheritance/ClassA.java
+++ ThreePillerOfJava/src/inheritance/ClassA.java
@@ -5,6 +5,6 @@ public class ClassA {
 	 int a=100;
 	 int b=200;
 	 int c=300;
-     
+     int d=400;
 	
 }
--- ThreePillerOfJava/src/inheritance/ClassB.java
+++ ThreePillerOfJava/src/inheritance/ClassB.java
@@ -3,17 +3,19 @@ package inheritance;
 public class ClassB extends ClassA {
 
 	
-	public void finalmodifier(int a,int b,int c) {
+	public void finalmodifier(int a,int b,int c,int d) {
 	this.a=a;
 	this.b=b;
 	this.c=c;
+	this.d=d;
 	System.out.println(a);
 	System.out.println(b);
 	System.out.println(c);
+	System.out.println(d);
 	}
 	public static void main(String[] args) {
 		ClassB kk=new ClassB();
-		kk.finalmodifier(50, 60, 70);
+		kk.finalmodifier(50, 60, 70,40);
 		
 		
 	}
